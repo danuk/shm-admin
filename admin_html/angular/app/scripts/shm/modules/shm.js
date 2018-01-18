0421638f9840048ddd80e057e2d755521c189896
Firsov Daniil <mail@danuk.ru>
2018-01-18 08:04:55 +0000

    dnk: Дочерние услуги

--- admin_html/angular/app/scripts/shm/modules/shm.js
+++ admin_html/angular/app/scripts/shm/modules/shm.js
@@ -1,6 +1,6 @@
 angular
 .module('shm', [])
-.service('shm', [ '$modal', function( $modal ) {
+.service('shm', [ '$modal', '$q', '$timeout', function( $modal, $q, $timeout ) {
     this.editJson = function(data) {
         return $modal.open({
             templateUrl: 'views/jsonEditor.html',
@@ -16,5 +16,72 @@ angular
             size: 'lg',
         });
     };
+    this.list_choises = function(data) {
+        var deferred = $q.defer();
+
+        $modal.open({
+            templateUrl: 'views/list_choises.html',
+            controller: function ($scope, $modalInstance) {
+                $scope.data = angular.copy(data);
+
+                $scope.btn_up_disabled = 1;
+                $scope.btn_down_disabled = 1;
+                $scope.btn_remove_disabled = 1;
+
+                $scope.add = function() {
+                    $scope.data.list_to.push( angular.copy( $scope.data.list_from[ $scope.data.from ] ) );
+                };
+
+                $scope.remove = function() {
+                    if ( $scope.data.to ) {
+                        $scope.data.list_to.splice( $scope.data.to[0], 1 );
+                        $scope.btn_remove_disabled = 1;
+                        $scope.btn_down_disabled = 1;
+                        $scope.btn_up_disabled = 1;
+                    }
+                };
+
+                $scope.$watch('data.to', function(newValue, oldValue) {
+                    if ( newValue === oldValue) return;
+
+                    var ItemSelected = $scope.data.to[0];
+                    $scope.btn_down_disabled = ( ItemSelected == $scope.data.list_to.length - 1 ) ? 1 : 0;
+                    $scope.btn_up_disabled = ItemSelected == 0 ? 1 : 0;
+                    $scope.btn_remove_disabled = 0;
+                }, true);
+
+                $scope.move = function(dir) {
+                    var selItemIndex = parseInt( $scope.data.to[0] );
+
+                    var array = $scope.data.list_to;
+                    var Item = array[ selItemIndex ];
+
+                    if ( dir === 'up' && selItemIndex > 0 ) {
+                            array[ selItemIndex ] = array[ selItemIndex - 1 ];
+                            array[ selItemIndex - 1 ] = Item;
+                            $scope.data.to[0] = selItemIndex - 1;
+                    }
+                    else if ( dir === 'down' && array.length > selItemIndex + 1 ) {
+                            array[ selItemIndex ] = array[ selItemIndex + 1 ];
+                            array[ selItemIndex + 1 ] = Item;
+                            $scope.data.to[0] = selItemIndex + 1;
+                    }
+                }
+
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
+                $scope.save = function () {
+                    $modalInstance.close( $scope.data );
+                };
+            },
+            size: 'lg',
+        }).result.then(function(data){
+            deferred.resolve( data.list_to );
+        });
+
+        return deferred.promise;
+    }
+
 }]);
 
--- admin_html/angular/app/scripts/shm/services.js
+++ admin_html/angular/app/scripts/shm/services.js
@@ -5,7 +5,9 @@ angular
   .controller('ShmServicesController', ['$scope', '$modal', 'shm', 'shm_request', function($scope, $modal, shm, shm_request) {
     'use strict';
 
-    $scope.url = 'admin/services.cgi';
+    var url = 'admin/services.cgi';
+    $scope.url = url;
+
     $scope.parent_key_id = 'service_id';
 
     $scope.columnDefs = [
@@ -15,34 +17,51 @@ angular
         {field: 'cost', displayName: 'Цена'},
     ];
 
-	$scope.openModal = function (title, row, size) {
-		var modalInstance = $modal.open({
-			templateUrl: 'edit.tmpl',
-			controller: function ($scope, $modalInstance, $modal) {
-				$scope.title = title;
-				$scope.data = angular.copy(row);
+    $scope.openModal = function (title, row, size) {
+        var modalInstance = $modal.open({
+            templateUrl: 'edit.tmpl',
+            controller: function ($scope, $modalInstance, $modal) {
+                $scope.title = title;
+                $scope.data = angular.copy(row);
+                $scope.data.children = [];
                 
                 // Load childs
                 shm_request('GET','/'+url, { parent: row.service_id } ).then(function(data) {
                     $scope.data.children = data;
                 });
 
-				$scope.cancel = function () {
-					$modalInstance.dismiss('cancel');
-				};
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
 
                 $scope.editJson = function(data) {
                     shm.editJson(data).result.then(function(json) {
                         $scope.data.config = json;
                     })
                 };
-			},
-			size: size,
-		});
-	}
+
+                $scope.editSubServices = function(service_id,children) {
+                    shm_request('GET','/'+url).then(function(data) {
+                        $scope.data.services = data;
+                        shm.list_choises({
+                            title: 'Управление дочерними услугами',
+                            list_caption_field: 'name',
+                            label_from: 'Услуги',
+                            list_from: $scope.data.services,
+                            label_to: 'Дочерние услуги',
+                            list_to: $scope.data.children,
+                        }).then(function(data){
+                            $scope.data.children = data;
+                        });
+                    });
+                };
+            },
+            size: size,
+        });
+    }
   
     $scope.row_dbl_click = function(row) {
-		$scope.openModal('Редактирование услуги', row, 'lg');
+        $scope.openModal('Редактирование услуги', row, 'lg');
     }
 
   }]);

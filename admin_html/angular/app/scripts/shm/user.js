eb985ef4cb73f93ab34e1b830f2853b30c684184
Daniil Firsov <mail@danuk.ru>
2018-09-30 15:58:01 +0000

    dnk: сохранение юзвера

--- admin_html/angular/app/scripts/shm/user.js
+++ admin_html/angular/app/scripts/shm/user.js
@@ -15,6 +15,9 @@ angular
 
     $scope.save = function() {
         console.warn( $scope.data );
+        shm_request('POST_JSON','/admin/user.cgi', $scope.data ).then(function(row) {
+            $location.path('/users');
+        })
     }
 
 }]);

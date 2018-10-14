c209bc9d0a4e4eb87006055a6ad5225273a6d150
Daniil Firsov <mail@danuk.ru>
2018-10-14 18:57:51 +0000

    dnk: user_service_edit

--- admin_html/angular/app/scripts/shm/user_services.js
+++ admin_html/angular/app/scripts/shm/user_services.js
@@ -1,7 +1,8 @@
 angular
   .module('shm_user_services', [
+    'angular-jsoneditor',
   ])
-  .controller('ShmUserServicesController', ['$scope', '$modal', function($scope, $modal) {
+  .controller('ShmUserServicesController', ['$scope', '$modal', 'shm', function($scope, $modal, shm) {
     'use strict';
 
     $scope.url = 'admin/user_services.cgi';
@@ -21,22 +22,50 @@ angular
         //{field: 'cost', displayName: 'Цена'},
     ];
 
-	$scope.openDemoModal = function (title, row, size) {
-		var modalInstance = $modal.open({
-			templateUrl: 'edit.tmpl',
-			controller: function ($scope, $modalInstance) {
-				$scope.title = title;
-				$scope.row = row;
-				$scope.cancel = function () {
-					$modalInstance.dismiss('cancel');
-				};
-			},
-			size: size,
-		});
-	}
-  
+    $scope.service_editor = function (title, row, size) {
+        return $modal.open({
+            templateUrl: 'views/user_service_edit.html',
+            controller: function ($scope, $modalInstance, $modal) {
+                $scope.title = title;
+                $scope.data = angular.copy(row);
+
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
+
+                $scope.save = function () {
+                    $modalInstance.close( $scope.data );
+                };
+
+                $scope.delete = function () {
+                    $modalInstance.dismiss('delete');
+                };
+
+                $scope.editJson = function(data) {
+                    shm.editJson(data).result.then(function(json) {
+                        $scope.data.settings = json;
+                    },function(cancel) {
+                    })
+                };
+            },
+            size: size,
+        });
+    }
+
+
     $scope.row_dbl_click = function(row) {
-		$scope.openDemoModal('Редактирование услуги', row, 'lg');
+        $scope.service_editor('Редактирование услуги', row, 'lg').result.then(function(data){
+            save_service( row, data );
+        }, function(resp) {
+            if ( resp === 'delete' ) {
+                shm_request('DELETE','/'+url+'?service_id='+row.service_id ).then(function() {
+                    $scope.gridOptions.data.splice(
+                        $scope.gridOptions.data.indexOf( row ),
+                        1
+                    );
+                })
+            }
+        });
     }
 
   }]);

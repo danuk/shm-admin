f1eaafb7e14e4c17570fe3f11d692724a143e32c
Daniil Firsov <mail@danuk.ru>
2018-12-30 19:19:54 +0000

    dnk: группы серверов

--- admin_html/angular/app/scripts/shm/servers_groups.js
+++ admin_html/angular/app/scripts/shm/servers_groups.js
@@ -1,10 +1,11 @@
 angular
   .module('shm_servers_groups', [
   ])
-  .controller('ShmServersGroupsController', ['$scope', function($scope) {
+  .controller('ShmServersGroupsController', ['$scope', '$modal', 'shm', 'shm_request', function($scope, $modal, shm, shm_request) {
     'use strict';
 
-    $scope.url = 'admin/servers_groups.cgi';
+    var url = 'admin/server_groups.cgi';
+    $scope.url = url;
 
     $scope.columnDefs = [
         {field: 'group_id'},
@@ -12,4 +13,62 @@ angular
         {field: 'type'},
     ];
    
+    $scope.editor = function (title, row, size) {
+        return $modal.open({
+            templateUrl: 'views/servers_groups_edit.html',
+            controller: function ($scope, $modalInstance, $modal) {
+                $scope.title = title;
+                $scope.data = angular.copy(row);
+
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
+
+                $scope.save = function () {
+                    $modalInstance.close( $scope.data );
+                };
+
+                $scope.delete = function () {
+                    $modalInstance.dismiss('delete');
+                };
+
+            },
+            size: size,
+        });
+    }
+
+    var save_service = function( row, save_data ) {
+        delete save_data.$$treeLevel;
+        shm_request('POST_JSON','/'+url, save_data ).then(function(new_data) {
+            angular.extend( row, new_data );
+        });
+    };
+
+    $scope.add = function() {
+        var row = {};
+
+        $scope.editor('Создание группы', row, 'lg').result.then(function(data){
+            shm_request('PUT_JSON','/'+url, data ).then(function(row) {
+                row.$$treeLevel = 0;
+                $scope.gridOptions.data.push( row );
+            });
+        }, function(cancel) {
+        });
+    };
+
+    $scope.row_dbl_click = function(row) {
+        $scope.editor('Редактирование группы', row, 'lg').result.then(function(data){
+            save_service( row, data );
+        }, function(resp) {
+            if ( resp === 'delete' ) {
+                shm_request('DELETE','/'+url+'?id='+row.group_id ).then(function() {
+                    $scope.gridOptions.data.splice(
+                        $scope.gridOptions.data.indexOf( row ),
+                        1
+                    );
+                })
+            }
+        });
+    }
+
   }]);

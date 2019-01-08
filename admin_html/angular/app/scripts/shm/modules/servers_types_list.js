0f0f2d65d6298f7e91f227147ad956f8f535d6b5
Daniil Firsov <mail@danuk.ru>
2019-01-08 18:05:23 +0000

    dnk: set default type for servers types

--- admin_html/angular/app/scripts/shm/modules/servers_types_list.js
+++ admin_html/angular/app/scripts/shm/modules/servers_types_list.js
@@ -7,6 +7,7 @@ angular.module('shm_servers_types_list', [
         data: '='
       },
       controller: function ($scope, $element, $attrs) {
+          if (!$scope.data) $scope.data = 'random';
       },
       templateUrl: "views/shm/modules/servers-types-list/select.html"
     }

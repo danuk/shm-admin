56623bc5df5afad940fdd53433582b7e4f02f1fb
Daniil Firsov <mail@danuk.ru>
2019-03-09 17:36:54 +0000

    dnk: update `spool` module

--- admin_html/angular/app/scripts/shm/spool.js
+++ admin_html/angular/app/scripts/shm/spool.js
@@ -1,19 +1,48 @@
 angular
   .module('shm_spool', [
   ])
-  .controller('ShmSpoolController', ['$scope', function($scope) {
+  .controller('ShmSpoolController', ['$scope', '$modal', 'shm', 'shm_request', function($scope, $modal, shm, shm_request) {
     'use strict';
 
     $scope.url = 'admin/spool.cgi';
 
     $scope.columnDefs = [
-        {field: 'id'},
-        {field: 'user_id'},
-        {field: 'user_service_id'},
-        {field: 'status'},
-        {field: 'created'},
-        {field: 'delayed'},
-        {field: 'responce'},
+        {
+            field: 'created',
+            width: 150,
+        },
+        {
+            field: 'user_service_id',
+            width: 150,
+        },
+        {
+            field: 'status',
+            width: 100,
+        },
+        {field: 'response'},
     ];
-   
+
+    $scope.row_dbl_click = function(row) {
+        return $modal.open({
+            templateUrl: 'views/spool_view.html',
+            controller: function ($scope, $modalInstance, $modal) {
+                $scope.title = 'Просмотри события';
+                $scope.data = angular.copy(row),
+                $scope.obj = {
+                   data: angular.copy(row.response),
+                   options: { mode: 'code' },
+                }
+
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
+
+                $scope.close = function () {
+                    $modalInstance.close( $scope.data );
+                };
+            },
+            size: 'lg',
+        });
+    }
+
   }]);

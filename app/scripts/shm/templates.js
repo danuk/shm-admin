dc2b4b793e14e8c549e372339a1f7cb4b9793299
Daniil Firsov <mail@danuk.ru>
2019-11-29 19:49:51 +0000

    dnk: fixup templates

--- app/scripts/shm/templates.js
+++ app/scripts/shm/templates.js
@@ -18,8 +18,8 @@ angular
                 };
 
                 $scope.save = function () {
-                    shm_request( $scope.data.id ? 'POST_JSON' : 'PUT_JSON','/'+url, $scope.data ).then(function() {
-                        $modalInstance.close( $scope.data );
+                    shm_request( $scope.data.id ? 'POST_JSON' : 'PUT_JSON','/'+url, $scope.data ).then(function(responce) {
+                        $modalInstance.close( responce.data );
                     });
                 };
 

99df7e35a01d126003fac85d225395353e019c6c
Daniil Firsov <mail@danuk.ru>
2021-10-11 16:09:36 +0000

    dnk: fixup withdraw column name

--- app/scripts/shm/withdraws.js
+++ app/scripts/shm/withdraws.js
@@ -8,7 +8,7 @@ angular
     $scope.url = url;
 
     $scope.columnDefs = [
-        {field: 'user_service_id', displayName: "id"},
+        {field: 'user_service_id', displayName: "usi"},
         {
             field: 'user_id',
             filter: { term: $scope.user.user_id },

e6fe5bbc84e5821f448c5750c893f5f13fe0b43e
Daniil Firsov <mail@danuk.ru>
2022-08-16 12:31:36 +0000

    dnk: rename `expired` to `expire`

--- app/scripts/shm/user_services.js
+++ app/scripts/shm/user_services.js
@@ -158,7 +158,7 @@ angular
                 };
             },
         },
-        {field: 'expired'},
+        {field: 'expire'},
         {field: 'withdraws.total', displayName: 'Стоимость'},
     ];
 
